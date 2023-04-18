9fd91d7c2c1228a062b6517f0a31e87f7490aba0
jvill171 <juanvillarreal64@yahoo.com>
2023-04-17 22:53:17 -0500

    Added __repr__

--- models.py
+++ models.py
@@ -11,6 +11,10 @@ class User(db.Model):
     '''User.'''
     __tablename__ = 'users'
 
+    def __repr__(self):
+        u = self
+        return f"<User username={u.username} password={u.password} email={u.email} first_name={u.first_name} last_name={u.last_name} >"
+
     username = db.Column(db.String(20), primary_key=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
