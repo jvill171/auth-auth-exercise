eca6cd105734ba4d4744a5af2aa12810f965c642
jvill171 <juanvillarreal64@yahoo.com>
2023-04-21 23:23:43 -0500

    Added feedback routes & fixed route descritpions

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, redirect, render_template, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db, db
+from models import connect_db, db, User, Feedback
+from forms import CreateUserForm, LoginUserForm, FeedbackForm, DeleteForm
+from werkzeug.exceptions import Unauthorized
+
+import sqlalchemy
+from psycopg2 import errors
 
 app = Flask(__name__)
 app.app_context().push()
@@ -12,7 +17,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///auth_auth'
 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_ECHO'] = False
 
 app.debug = True
 debug=DebugToolbarExtension(app)
@@ -31,40 +36,163 @@ def homepage():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     '''
-    ['GET']
-    Show a form that when submitted will register/create a user.
-    This form accepts a username, password, email, first_name, and last_name.
-    Make sure you are using WTForms and that your password input hides the characters that the user is typing!
-
-    ['POST']
-    Process the registration form by adding a new user. Then redirect to /secret
+    Display a form for users to register a new account with.
+    Successful registration: create user in DB, add username to session['username'], and redirect to /users/<username>
     '''
-    return render_template('register.html')
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
+    
+    form = CreateUserForm()
+
+    if form.validate_on_submit():
+        data = {k: v for k, v in form.data.items() if k != "csrf_token"}
+        new_user = User.register(**data)
+        try:
+            db.session.commit()
+            session['username'] = new_user.username
+            return redirect(f'/users/{new_user.username}')
+        except sqlalchemy.exc.IntegrityError as sqla_error:
+            try:
+                raise sqla_error.orig
+            except (errors.UniqueViolation):
+                flash("An account with that Username/Email already exists!", 'danger')
+
+    return render_template('users/register.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     '''
-    ['GET']
-    Show a form that when submitted will login a user. This form should accept a username and a password.
-    Make sure you are using WTForms and that your password input hides the characters that the user is typing!
-
-    ['POST']
-    Process the login form, ensuring the user is authenticated and going to /secret if so.
+    Display a form for users to log in with.
+    Successful login: add username to session['username'] & redirect to /users/<username>
     '''
-    return render_template('login.html')
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
+    
+    form = LoginUserForm()
 
-@app.route('/secret')
-def secret():
+    if form.validate_on_submit():
+        data = {k: v for k, v in form.data.items() if k != "csrf_token"}
+        user = User.authenticate(**data)
+
+        if(user):
+            session['username'] = user.username
+            return redirect(f'/users/{user.username}')
+        else:
+            form.username.errors = ["Invalid username or password"]
+            return render_template("users/login.html", form=form)
+    
+    return render_template('users/login.html', form=form)
+
+@app.route('/users/<username>')
+def secret(username):
     '''
-    Return the text “You made it!” (don't worry, we'll get rid of this soon)
+    Display info about the user, including any of the user's feedback 
+    Login Required.
     '''
-    html="<h1>You made it!</h1>"
-    return html
+    if "username" not in session or username != session['username']:
+        raise Unauthorized()
+    
+    user = User.query.get_or_404(username)
+    form=DeleteForm()  
+    return render_template('users/user.html', user=user, form=form)
 
-# @app.route('/')
-# def homepage():
-#     return render_template('base.html')
+@app.route('/logout')
+def logout():
+    '''Log out currently logged in user'''
+    try:
+        session.pop('username')
+    except Exception:
+        # User tried to access /logout route while logged out
+        pass
+    return redirect('/login')
 
-# @app.route('/')
-# def homepage():
-#     return render_template('base.html')
+@app.route('/users/<username>/delete', methods=['POST'])
+def delete_user(username):
+    '''
+    Delete the user from the DB & delete all of their feedback.
+    Clear any user information in the session and redirect to /
+    Login Required.
+    '''
+    if "username" not in session or username != session['username']:
+        raise Unauthorized()
+    
+    user = User.query.get_or_404(username)
+    db.session.delete(user)
+    db.session.commit()
+    session.pop('username')
+    
+    return redirect('/login')
+
+@app.route('/users/<username>/feedback/add', methods=['GET', 'POST'])
+def add_feedback(username):
+    '''Display a form to add feedback. On submission, adds to DB and redirect to /users/<username>
+         Login Required.'''
+    if "username" not in session or username != session['username']:
+        raise Unauthorized()
+    
+    form = FeedbackForm()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        
+        new_feedback = Feedback(
+            title = title,
+            content = content, 
+            username = username
+        )
+        
+        db.session.add(new_feedback)
+        db.session.commit()
+        
+        return redirect(f'/users/{username}')
+        
+    return render_template('feedback/add-feedback.html', form=form)
+
+@app.route('/feedback/<int:feedback_id>/update', methods=['GET', 'POST'])
+def update_feedback(feedback_id):
+    '''
+    Displays a form to edit feedback. When submitted, updates DB and redirects to /users/<username>
+    Login Required.
+    '''
+    fb = Feedback.query.get_or_404(feedback_id)
+
+    if "username" not in session or fb.username != session['username']:
+        raise Unauthorized()
+    
+    form = FeedbackForm(obj=fb)
+
+    if form.validate_on_submit():
+        fb.title = form.title.data
+        fb.content= form.content.data
+        db.session.commit()
+        return redirect(f'/users/{fb.username}')
+
+    return render_template('/feedback/edit-feedback.html', form=form)
+
+@app.route('/feedback/<int:feedback_id>/delete', methods=['POST'])
+def delete_feedback(feedback_id):
+    '''
+    Deletes a specific feedback & redirects to /users/<username>
+    Login Required.
+    '''
+    fb = Feedback.query.get_or_404(feedback_id)
+
+    if "username" not in session or fb.username != session['username']:
+        raise Unauthorized()
+    
+    user = fb.username
+    db.session.delete(fb)
+    db.session.commit()
+
+    return redirect(f'/users/{user}')
+
+@app.errorhandler(401)
+def page_err_401(error):
+    '''Renders 401 error page'''
+    return render_template('401.html', err=error),401
+
+@app.errorhandler(404)
+def page_err_404(error):
+    '''Renders 404 error page'''
+    return render_template('404.html', err=error),404
