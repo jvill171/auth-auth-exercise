b95f085dc26501e843b21a016e5357f68e3c70ff
jvill171 <juanvillarreal64@yahoo.com>
2023-04-21 23:21:39 -0500

    Fixed input types & added FeedbackForm & DeleteForm

--- forms.py
+++ forms.py
@@ -1,16 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import InputRequired
+from wtforms import StringField, PasswordField, EmailField, TextAreaField
+from wtforms.validators import InputRequired, Length
 
 class CreateUserForm(FlaskForm):
     '''Form for registering or creating a new user.'''
-    username    = StringField("Username", validators=[InputRequired('Username Required')])
-    password    = StringField("Password", validators=[InputRequired('Password Required')])
-    email       = StringField("Email", validators=[InputRequired('Email Required')])
-    first_name  = StringField("First name", validators=[InputRequired('First name Required')])
-    last_name   = StringField("Last name", validators=[InputRequired('Last name Required')])
+    username   = StringField("Username", validators=[InputRequired('Username Required')])
+    password   = PasswordField("Password", validators=[InputRequired('Password Required')])
+    email      = EmailField("Email", validators=[InputRequired('Email Required')])
+    first_name = StringField("First name", validators=[InputRequired('First name Required')])
+    last_name  = StringField("Last name", validators=[InputRequired('Last name Required')])
 
 class LoginUserForm(FlaskForm):
     '''Form for logging in a user.'''
-    username    = StringField("Username", validators=[InputRequired('Username Required')])
-    password    = StringField("Password", validators=[InputRequired('Password Required')])
+    username = StringField("Username", validators=[InputRequired('Username Required')])
+    password = PasswordField("Password", validators=[InputRequired('Password Required')])
+    
+class FeedbackForm(FlaskForm):
+    '''Form for adding feedback.'''
+    title   = StringField("Title", validators=[InputRequired(), Length(max=100)])
+    content = TextAreaField("Feedback", validators=[InputRequired()])
+
+class DeleteForm(FlaskForm):
+    '''Intentionally blank form used for deletion of users/feedback'''
